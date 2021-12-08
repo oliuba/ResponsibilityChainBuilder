ccabce84f025a8ef2e9934b6c123ae72050229f1
oliuba <olgaluba3@gmail.com>
2021-12-09 00:33:42 +0200

    Remove import in test

--- src/test/java/atm/ATMTest.java
+++ src/test/java/atm/ATMTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ATMTest {
